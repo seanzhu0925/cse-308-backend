00cd0b5c17723b16c2292e726b184c5a57778337
Sean <seanzhu0925@gmail.com>
2018-10-14 20:32:10 -0400

    controller change

--- src/main/java/com/cse/java/cse308/controller/Controller.java
+++ src/main/java/com/cse/java/cse308/controller/Controller.java
@@ -1,16 +1,14 @@
 package com.cse.java.cse308.controller;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,15 +24,15 @@ import com.cse.java.cse308.service.UserService;
 @RestController
 @CrossOrigin(origins = "http://http://localhost:8000/")
 public class Controller {
-	
-	 @Autowired
-	 private PersonService personService;
-	 @Autowired
-	 private PersonRepository personRepository;
-	 @Autowired
-	 private UserRepository userRepository;
-	 @Autowired
-	 private UserService userService;
+
+	@Autowired
+	private PersonService personService;
+	@Autowired
+	private PersonRepository personRepository;
+	@Autowired
+	private UserRepository userRepository;
+	@Autowired
+	private UserService userService;
 
 	@RequestMapping("/")
 	public String getToHomePage() {
@@ -46,11 +44,11 @@ public class Controller {
 	public List<User> getUsers() {
 		return userService.getAllUsers();
 	}
-	
+
 	@PostMapping("/create/user")
 	public boolean creaetNewUser(@RequestBody UserDetailsRequestModel requestUserDetails) {
 		User user = userRepository.findByUserName(requestUserDetails.getUserName());
-		if(user == null) {
+		if (user == null) {
 			user = new User();
 			user.setUserName(requestUserDetails.getUserName());
 			user.setPassword(requestUserDetails.getPassword());
@@ -60,86 +58,97 @@ public class Controller {
 			user.setWorkStatus(Integer.parseInt(requestUserDetails.getWorkStatus()));
 			userService.saveUser(user);
 			return true;
-		}else {
+		} else {
 			return false;
-		}		
+		}
 	}
-	
-	@RequestMapping("/delete/user/{username}")
-	public String deleteUser(@PathVariable String username) {
-		
+
+	@PutMapping("/update/user")
+	public boolean updateUser(@RequestBody UserDetailsRequestModel requestUserDetails) {
+		User user = userRepository.findByUserName(requestUserDetails.getUserName());
+		if (user == null) {
+			return true;
+		} else {
+			user.setUserName(requestUserDetails.getUserName());
+			user.setPassword(requestUserDetails.getPassword());
+			user.setCanvasserRole(Integer.parseInt(requestUserDetails.getCanvasserRole()));
+			user.setManagerRole(Integer.parseInt(requestUserDetails.getManagerRole()));
+			user.setSystemRole(Integer.parseInt(requestUserDetails.getSystemRole()));
+			user.setWorkStatus(Integer.parseInt(requestUserDetails.getWorkStatus()));
+			userService.saveUser(user);
+			return false;
+		}
+	}
+
+	@DeleteMapping("/delete/user/{username}")
+	public void deleteUser(@PathVariable String username) {
 		userRepository.deleteById(username);
-		
-		return "User " + username + " is deleted.";
 	}
-	
+
 	@RequestMapping("/update/user/man/1/{username}")
 	public String updateUserMan1(@PathVariable String username) {
 		User user = userRepository.findByUserName(username);
 		user.setManagerRole(1);
 		userRepository.save(user);
-		
 		return "ManagerRole updated to 1 for " + username;
 	}
-	
+
 	@RequestMapping("/update/user/man/0/{username}")
 	public String updateUserMan0(@PathVariable String username) {
 		User user = userRepository.findByUserName(username);
 		user.setManagerRole(0);
 		userRepository.save(user);
-		
+
 		return "ManagerRole updated to 0 for " + username;
 	}
-	
+
 	@RequestMapping("/update/user/can/1/{username}")
 	public String updateUserCan1(@PathVariable String username) {
 		User user = userRepository.findByUserName(username);
 		user.setCanvasserRole(1);
 		userRepository.save(user);
-		
+
 		return "CanvasserRole updated to 1 for " + username;
 	}
-	
+
 	@RequestMapping("/update/user/can/0/{username}")
 	public String updateUserCan0(@PathVariable String username) {
 		User user = userRepository.findByUserName(username);
 		user.setCanvasserRole(0);
 		userRepository.save(user);
-		
+
 		return "CanvasserRole updated to 0 for " + username;
 	}
-	
-	
+
 	@RequestMapping("/update/user/sys/1/{username}")
 	public String updateUserSys1(@PathVariable String username) {
 		User user = userRepository.findByUserName(username);
 		user.setSystemRole(1);
 		userRepository.save(user);
-		
+
 		return "SystemRole updated to 1 for " + username;
 	}
-	
+
 	@RequestMapping("/update/user/sys/0/{username}")
 	public String updateUserSys0(@PathVariable String username) {
 		User user = userRepository.findByUserName(username);
 		user.setSystemRole(0);
 		userRepository.save(user);
-		
+
 		return "SystemRole updated to 0 for " + username;
 	}
 
 	@GetMapping("/get/person/{ssn}")
 	public boolean findPerson(@PathVariable String ssn) {
-		
+
 		Person per = personRepository.findTop1BySsn(ssn);
 		return per != null ? true : false;
 	}
-	
+
 	@GetMapping("/get/user/{username}")
 	public User findUser(@PathVariable String username) {
 		User user = userRepository.findByUserName(username);
 		return user;
 	}
-	
 
 }
